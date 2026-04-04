5c736a1593e677456a508fc7d0380e55d52d4fef
Logic <logiclrd@laliari.logiclrd.cx>
2026-04-04 16:49:34 -0500

    Updated DQD.ForkPTY.Native.FreeBSD to not assume the indices of control characters in termios.c_cc: - Added control_chars.c. Updated control_chars.c/control_chars.h to export a const array of mappings using the index macros from system headers. - Updated forkpty.c to fill in term.c_cc using the new array exported from control_chars.c. - Added exports.map to control what symbols are exported from the final shared object build result. - Updated Makefile to build all .c files to .o files and to link them using the exports.map version script. Added an updated build of libDQD.ForkPTY.Native.FreeBSD.so to NativeBinaries.

--- DQD.ForkPTY.Native.FreeBSD/control_chars.h
+++ DQD.ForkPTY.Native.FreeBSD/control_chars.h
@@ -1,24 +1,32 @@
 #ifndef CONTROL_CHARS_H
 #define CONTROL_CHARS_H
 
-enum char_t
+typedef enum : char
 {
-        INTR = 3,
-        QUIT = 28,
-        ERASE = 127,
-        KILL = 21,
-        EOF_c = 4,
-        TIME = 0,
-        MIN = 1,
-        SWITCH_none = 0,
-        START = 19,
-        STOP = 17,
-        SUSP = 26,
-        EOL_NUL = 0,
-        REPRINT = 18,
-        DISCARD = 15,
-        WERASE = 23,
-        LNEXT = 22,
-};
+	INTR = 3,
+	QUIT = 28,
+	ERASE = 127,
+	KILL = 21,
+	EOF_c = 4,
+	TIME = 0,
+	MIN = 1,
+	SWITCH_none = 0,
+	START = 19,
+	STOP = 17,
+	SUSP = 26,
+	EOL_NUL = 0,
+	REPRINT = 18,
+	DISCARD = 15,
+	WERASE = 23,
+	LNEXT = 22,
+} char_t;
+
+typedef struct
+{
+	int index;
+	char_t ch;
+} control_char_mapping_t;
+
+extern const control_char_mapping_t ControlChars[];
 
 #endif // CONTROL_CHARS_H
